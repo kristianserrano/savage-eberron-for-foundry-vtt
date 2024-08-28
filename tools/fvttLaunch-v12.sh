e35f573af51a7628a2fe13b1c2d91185cafea91e
Kristian Serrano <kristianserrano@gmail.com>
2024-08-28 12:59:42 -0400

    Delete tools/fvttLaunch-v12.sh

--- tools/fvttLaunch-v12.sh
+++
@@ -1,3 +0,0 @@
-fvtt configure set "installPath" "/Users/kristianserrano/Documents/Projects/FoundryVTT-12"
-fvtt configure set "dataPath" "/Users/kristianserrano/Library/Application Support/FoundryVTT-v12"
-fvtt launch --noupnp --noipdiscovery --hotReload
