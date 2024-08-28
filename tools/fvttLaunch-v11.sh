61eb16bbfcc1d2ca1f7960ced6312cf00ae9ce69
Kristian Serrano <kristianserrano@gmail.com>
2024-08-28 12:59:27 -0400

    Delete tools/fvttLaunch-v11.sh

--- tools/fvttLaunch-v11.sh
+++
@@ -1,3 +0,0 @@
-fvtt configure set "installPath" "/Users/kristianserrano/Documents/Projects/FoundryVTT-11"
-fvtt configure set "dataPath" "/Users/kristianserrano/Library/Application Support/FoundryVTT-v11"
-fvtt launch --noupnp --noupdate --noipdiscovery --hotReload --world eberron-for-savage-worlds
